d27ffe25d6d227686fab70efaf890fd454fe6f82
Raptor95 <bagdadi_kashif@yahoo.co.in>
2018-12-07 06:45:41 -0500

    Added CORS

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtokens");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user/user.model.server");
 
