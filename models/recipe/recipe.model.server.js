880722b14cc00da9f1d2d3e6ec4c05087e6b17e2
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-12-08 22:15:14 -0500

    kashif chutiya

--- models/recipe/recipe.model.server.js
+++ models/recipe/recipe.model.server.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const recipeSchema = require('./user.schema.server');
+const recipeSchema = require('./recipe.schema.server');
 const recipeModel = mongoose.model('Recipe',recipeSchema);
 module.exports = recipeModel;
