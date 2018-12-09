2b8f5aa9938cbef70735bb827212fb288b1e780a
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-12-08 22:12:11 -0500

    changes for CORS

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 //CORS
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Origin", "https://dbms-recipe-app-frontend.herokuapp.com");
 res.header("Access-Control-Allow-Headers",
 "Origin, X-Requested-With, Content-Type, Accept");
 res.header("Access-Control-Allow-Methods",
-"GET, POST, PUT, DELETE");
+"GET, POST, PUT, DELETE, OPTIONS");
 res.header("Access-Control-Allow-Credentials", "true");
 next();
 });
