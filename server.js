4500477b69dc1b24fc067484acf998de42c39f2e
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-12-08 21:43:53 -0500

    changed heroku CORS url

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://dbms-recipe-app-frontend.herokuapp.com/home");
+    res.setHeader("Access-Control-Allow-Origin", "https://dbms-recipe-app-frontend.herokuapp.com");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
